391c8616f153c67fb0cf2f76c0b867870e1bfbc5
gloween <mmarevalop.smmc@gmail.com>
2026-02-13 08:18:42 -0600

    Estado finalizado

--- SistemaContinuo.py
+++ SistemaContinuo.py
@@ -1,32 +1,14 @@
 import random
 import time
 
-libre = 10
-ocupado = 0
-
-print("Estado del sistema:")
-print("-" * 20)
+print("Inicia sistema")
 
 while True:
     try:
-        if random.choice([True, False]):  
-            cambio = random.randint(-1, 1)
-            nuevo_ocupado = ocupado + cambio
-            if 0 <= nuevo_ocupado <= 10:
-                ocupado = nuevo_ocupado
-                if ocupado > 0:
-                    print("ocupado")
-                else:
-                    print("libre")
-        else:  
-            cambio = random.randint(-1, 1)
-            nuevo_libre = libre + cambio
-            if 0 <= nuevo_libre <= 10:
-                libre = nuevo_libre
-                if libre < 10:
-                    print("ocupado")
-                else:
-                    print("libre")
+        if random.random() < 0.5:
+            print("ocupado")
+        else:
+            print("libre")
         
         time.sleep(0.5)
         
